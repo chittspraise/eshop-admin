aea8c76e1e55f1fd28a6620472edce8fa59766f6
chittspraise <praizechitts@gmail.com>
2024-12-27 11:48:16 +0200

    Disable ESLint checks for production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
- images:{
-  remotePatterns:[
-{
-  protocol: 'https',
-  hostname:'nlwtuqszbnuvrmhjcbcp.supabase.co',
-},
-{
-  protocol: 'https',
-  hostname: 'images.unsplash.com',
-}
- ]
- }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nlwtuqszbnuvrmhjcbcp.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ]
+  },
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Disable TypeScript during production builds
+    ignoreBuildErrors: true,
+  }
 };
+
 export default nextConfig;
